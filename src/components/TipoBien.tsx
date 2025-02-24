3d4882c6b79a3c60e1e2c00d1f9fbe8180e8dc7c
Juan Ramirez <ciscojuan@gmail.com>
2025-02-24 10:23:11 -0500

    initial data are loaded at application start, and when tipoBien is selected

--- src/components/TipoBien.tsx
+++ src/components/TipoBien.tsx
@@ -6,25 +6,25 @@ import { serverActions } from ".";
 
 export const TipoBien = () => {
   const [nombreBien, setNombreBien] = useState("");
-  const { idBien, setIdBien } = useContext(bienContext);
+  const { idBien, setIdBien, tipoBien, setTipoBien } = useContext(bienContext);
 
   useEffect(() => {
     const getIdBien = async () => {
-      const id = await serverActions.getBienId(nombreBien).then((id) => {
+      const id = await serverActions.getBienId(tipoBien).then((id) => {
         setIdBien(id?.bien_id || "");
       });
       return id;
     };
     getIdBien();
-  }, [nombreBien]);
+  }, [tipoBien]);
 
   return (
     <div className="w-full max-w-sm min-w-[200px]">
       <div className="relative">
         <select
           className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-md border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer"
-          value={nombreBien}
-          onChange={({ target }) => setNombreBien(target.value)}
+          value={tipoBien}
+          onChange={({ target }) => setTipoBien(target.value)}
         >
           <option value="Sol naciente">Sol Naciente</option>
           <option value="Acapulco">Acapulco</option>
